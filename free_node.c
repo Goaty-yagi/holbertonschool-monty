f6fb8a4a22c571f91ba28e5093e8017d088cdd55
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-01-04 13:27:11 +1100

    fix(test): fix memory error

--- free_node.c
+++ free_node.c
@@ -3,20 +3,18 @@
 void free_all_node(stack_type **stack)
 {
     stack_type *current = stack[0];
-    if (!stack[0])
+    stack_type *next_node = NULL;
+
+    if (!current)
         return;
 
-    while (current != NULL)
-    {
-        if (current->next)
-        {
-            current = current->next;
-            free(current->prev);
-        }
-        else
-        {
-            free(current);
-            current = current->next;
-        }
+    while (current != NULL) {
+        next_node = current->next;
+
+        free(current);
+
+        current = next_node;
     }
+
+    *stack = NULL;
 }
