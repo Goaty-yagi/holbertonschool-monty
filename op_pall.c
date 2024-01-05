2ffcfb473d6708f1abf1d608f775744919639bba
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-01-05 15:37:40 +1100

    docs: add betty docs

--- op_pall.c
+++ op_pall.c
@@ -1,5 +1,15 @@
 #include "monty.h"
 
+/**
+ * op_pall - Prints all elements in the stack
+ * @stack: Pointer to a pointer to the top of the stack
+ * @line_number: Line number in the file being processed (unused)
+ *
+ * This function prints all the integer values stored in the stack,
+ * starting from the top element and moving downwards. It traverses
+ * the stack, printing each node's 'n' value followed by a newline.
+ */
+
 void op_pall(stack_type **stack, unsigned int line_number)
 {
 	stack_type *current = *stack;
--- op_push.c
+++ op_push.c
@@ -1,5 +1,16 @@
 #include "monty.h"
-#include <ctype.h>
+
+/**
+ * op_push - Adds a new element to the top of the stack
+ * @stack: Pointer to a pointer to the top of the stack
+ * @line_number: Line number in the file being processed
+ *
+ * This function creates a new node and adds it to the top of the stack.
+ * It assigns the given line_number as the value of the new node. If the
+ * allocation fails or if the line_number is invalid, it displays an error
+ * message and exits with a failure status. The new node becomes the top
+ * element of the stack.
+ */
 
 void op_push(stack_type **stack, unsigned int line_number)
 {
