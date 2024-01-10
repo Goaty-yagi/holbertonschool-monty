21a5ba29c823c08ab16a31b2dc0fea2d4196192c
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-01-10 16:35:16 +1100

    feat: add swap function

--- monty.h
+++ monty.h
@@ -59,6 +59,7 @@ void op_push(stack_type **stack, unsigned int line_number);
 void op_pall(stack_type **stack, unsigned int line_number);
 void op_pint(stack_type **stack, unsigned int line_number);
 void op_pop(stack_type **stack, unsigned int line_number);
+void op_swap(stack_type **stack, unsigned int line_number);
 void define_global_var(char *line);
 void free_all_node(stack_type **stack);
 
---
+++ op_swap.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+ * op_swap - Swaps the top two elements of the stack
+ * @stack: Pointer to a pointer to the top of the stack
+ * @line_number: Line number in the file being processed
+ *
+ * This function swaps the positions of the top two elements in the stack.
+ * If the stack has fewer than two elements, it displays an error message
+ * and exits with a failure status. Otherwise, it rearranges the pointers
+ * to perform the swap operation.
+ */
+
+void op_swap(stack_type **stack, unsigned int line_number)
+{
+	stack_type *temp = *stack;
+
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		free_all_node(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	*stack = temp->next;
+	(*stack)->prev = NULL;
+	temp->prev = *stack;
+	temp->next = (*stack)->next;
+	(*stack)->next = temp;
+}
