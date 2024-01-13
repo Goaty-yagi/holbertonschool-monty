10c7b33a22fc38cf87fb76b63ef4b5d08cb94cac
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-01-13 15:56:25 +1100

    fix: case empty line

--- op_push.c
+++ op_push.c
@@ -14,7 +14,7 @@ int parse_input(char *str)
 
 	while (counter < len)
 	{
-		if(str[counter] == '-' && counter == 0)
+		if (str[counter] == '-' && counter == 0)
 		{
 			counter = counter + 1;
 			continue;
@@ -48,6 +48,7 @@ int parse_input(char *str)
 void op_push(stack_type **stack, unsigned int line_number)
 {
 	stack_type *new_node;
+
 	if (!parse_input(file_s.VALUE) || file_s.VALUE[0] == '\0')
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
